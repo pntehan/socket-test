5962b2eb27495f4630e3f03ee16c31fec61580df
pntehan <2390003207@qq.com>
2019-02-12 15:36:25 +0800

    socket客户端

--- socket_client.py
+++ socket_client.py
@@ -3,10 +3,12 @@ import socket
 client = socket.socket()
 ip_port = ('127.0.0.1', 8880)
 client.connect(ip_port)
+name = client.recv(1024)
+client.send('Names'.encode())
 while True:
 	data = client.recv(1024)
-	print(data.decode())
+	print('{}: {}'.format(name.decode(), data.decode()))
+	if data == b'exit':
+		break
 	msg = input('输入要发送的消息:')
 	client.send(msg.encode())
-	if msg == 'exit':
-		break
